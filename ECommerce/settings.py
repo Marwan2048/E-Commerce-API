afaef86a72a8fe79b23684e86586adfa80911b28
Marwan Elkarif <marwanelkarf@gmail.com>
2026-09-06 21:58:22 +0300

    Djoser + JWT

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Shop',
+    'rest_framework',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -124,4 +127,14 @@ MAILERS = {
     'default': {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+DJOSER = {
+    'USER_CREATE_PASSWORD_RETYPE': True,
 }
